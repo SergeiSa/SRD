0f760909723f132b5e8e620caeb11fe720be4947
SergeiSa <sergey89mtkgtu@mail.ru>
2021-04-22 15:13:24 +0300

    new preserialization

--- Model Handlers/SRD_get_handler__dynamics_generalized_coordinates_model.m
+++ Model Handlers/SRD_get_handler__dynamics_generalized_coordinates_model.m
@@ -19,30 +19,37 @@ Handler_dynamics_generalized_coordinates_model.State.description = description;
 Handler_dynamics_generalized_coordinates_model.dof_configuration_space_robot = description.dof_configuration_space_robot;
 Handler_dynamics_generalized_coordinates_model.dof_control                   = description.dof_control;
     
+Handler_dynamics_generalized_coordinates_model.SerializationPrepNeeded = true;
+Handler_dynamics_generalized_coordinates_model.PreSerializationPrepFunction  = @PreSerializationPrepFunction;
 if Casadi
-    Handler_dynamics_generalized_coordinates_model.SerializationPrepNeeded = true;
     
-    Handler_dynamics_generalized_coordinates_model.PostSerializationPrepFunction = @PostSerializationPrepFunction;
-    Handler_dynamics_generalized_coordinates_model.PreSerializationPrepFunction  = @PreSerializationPrepFunction;
-
+    Handler_dynamics_generalized_coordinates_model.PostSerializationPrepFunction = @PostSerializationPrepFunction_casadi;
 else
-    if ~isempty(description.Path)
-        current_dir = pwd;
-        cd(description.Path);
-    end
-    
-    %H*ddq + c = T*u
-    Handler_dynamics_generalized_coordinates_model.get_joint_space_inertia_matrix = str2func(description.FunctionName_H);
-    Handler_dynamics_generalized_coordinates_model.get_bias_vector = str2func(description.FunctionName_c);
-    Handler_dynamics_generalized_coordinates_model.get_control_map = str2func(description.FunctionName_T);
-    
-    if ~isempty(description.Path)
-        cd(current_dir);
-    end
+    Handler_dynamics_generalized_coordinates_model.PostSerializationPrepFunction = @PostSerializationPrepFunction_m;
 end
 
         
-    function PostSerializationPrepFunction(Handler_dynamics_generalized_coordinates_model)
+    function PostSerializationPrepFunction_m(Handler_dynamics_generalized_coordinates_model)
+        
+        if ~isempty(Handler_dynamics_generalized_coordinates_model.State.description.Path)
+            current_dir = pwd;
+            cd(Handler_dynamics_generalized_coordinates_model.State.description.Path);
+        end
+        
+        %H*ddq + c = T*u
+        Handler_dynamics_generalized_coordinates_model.get_joint_space_inertia_matrix = ...
+            str2func(Handler_dynamics_generalized_coordinates_model.State.description.FunctionName_H);
+        Handler_dynamics_generalized_coordinates_model.get_bias_vector = ...
+            str2func(Handler_dynamics_generalized_coordinates_model.State.description.FunctionName_c);
+        Handler_dynamics_generalized_coordinates_model.get_control_map = ...
+            str2func(Handler_dynamics_generalized_coordinates_model.State.description.FunctionName_T);
+        
+        if ~isempty(Handler_dynamics_generalized_coordinates_model.State.description.Path)
+            cd(current_dir);
+        end
+        
+    end
+    function PostSerializationPrepFunction_casadi(Handler_dynamics_generalized_coordinates_model)
         import casadi.*
         
         so_function_name = [Handler_dynamics_generalized_coordinates_model.State.description.Path, ...
@@ -58,6 +65,8 @@ end
         Handler_dynamics_generalized_coordinates_model.get_control_map                = @(q)    full(evalf(external_T(q)));
   
     end
+
+
     function PreSerializationPrepFunction(Handler_dynamics_generalized_coordinates_model)
         
         Handler_dynamics_generalized_coordinates_model.get_joint_space_inertia_matrix = [];
